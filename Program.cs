56622c37109a17a24b9e455335e3aaf343d50fc4
bentan2013 <tanben1986@gmail.com>
2020-02-09 22:39:28 +0800

    upate

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace my_first_dotnet_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = (100 / 2.3);
+            var b = (100 / 2.3);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
